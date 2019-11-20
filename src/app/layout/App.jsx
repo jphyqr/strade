7dbd1a704b470aa84c47927f48e6da71c3fdb210
John Hashem <johnhashem@Johns-MBP.hitronhub.home>
2019-11-19 21:07:24 -0600

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react'
+
+ class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>
+    TradeSmart
+      </h1>
+      </div>
+    );
+  }
+}
+
+
+
+export default App;
